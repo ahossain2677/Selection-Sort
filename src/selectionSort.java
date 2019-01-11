5d4cdd85f1d184debcdc2bab1901e0acc7bc5fb5
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2019-01-11 08:44:45 -0500

    Update selectionSort.java

--- src/selectionSort.java
+++ src/selectionSort.java
@@ -1,18 +1,31 @@
-public class SelectionSort {
-    static void sort(int arr[], int ind) {
+public class SelectionSort
+{
+    static int sort(int arr[], int ind)
+    {
         int minInd = ind;
-                for (int i = ind; i <arr.length; i++)
-                {
-                    if (arr[i] < arr[minInd])
-                    {
-                        swap(arr[],i,minInd);{
-                        minInd = i;
-                    }
 
-                    return minInd;
+        for (int i = ind; i <arr.length; i++)
+        {
+            if (arr[i] < arr[minInd])
+            {
+                swap(arr,i,minInd);
+                {
+                minInd = i;
                 }
 
             }
 
+        }
+        return minInd;
+    }
+    // Swap method to switch vales arr[i] an arr[j]
+    public static void swap(int[] arr, int i , int j)
+    {
+        int x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
 
-        }}
+    }
+    public static void int minInd
+        
+}
